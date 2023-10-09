1802119be5c0a3000b43b9cc09661b9ea8027354
jhonatancunha <jhonatancunha@alunos.utfpr.edu.br>
2023-10-09 20:29:11 -0300

    feat: animando jogador

--- Player/player.gd
+++ Player/player.gd
@@ -10,6 +10,19 @@ extends CharacterBody2D
 var gravity: int = ProjectSettings.get_setting("physics/2d/default_gravity")
 var already_did_double_jump: bool = false
 
+@onready var animationPlayer = $AnimationPlayer
+@onready var sprite = $Sprite2D
+
+
+func update_animation(direction):
+	if is_on_floor():
+		if direction == 0:
+			animationPlayer.play("idle")
+		else:
+			animationPlayer.play("walk")
+
+func _ready() -> void:
+	pass
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -17,6 +30,7 @@ func _physics_process(delta: float) -> void:
 		velocity.y += gravity * delta
 	else:
 		already_did_double_jump = false
+		
 
 	# Handle Jump.
 	if Input.is_action_just_pressed("jump"):
@@ -30,9 +44,15 @@ func _physics_process(delta: float) -> void:
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("left", "right")
+	
+	if direction !=0:
+		sprite.flip_h = (direction == -1);
+	
 	if direction:
 		velocity.x = direction * speed
 	else:
 		velocity.x = move_toward(velocity.x, 0, speed)
 
 	move_and_slide()
+	update_animation(direction)
+	
