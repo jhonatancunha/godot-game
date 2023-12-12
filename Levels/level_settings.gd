8b1433df9045e1f140150dd1dc66275e5a79f520
jhonatancunha <jhonatancunha@alunos.utfpr.edu.br>
2023-12-11 21:33:42 -0300

    fix: change game name

--- Levels/level_settings.gd
+++ Levels/level_settings.gd
@@ -1,11 +1,6 @@
 extends Node2D
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	if not OS.has_feature("mobile"):
-		Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
