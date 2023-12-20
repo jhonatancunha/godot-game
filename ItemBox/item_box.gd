bfec1371d624efc490eb95834fc744269471c2e4
JessePires <jessepires2010@gmail.com>
2023-12-19 23:46:00 -0300

    feat: create credits screen and add sounds

--- ItemBox/item_box.gd
+++ ItemBox/item_box.gd
@@ -18,7 +18,7 @@ func _on_area_2d_area_entered(area: Area2D) -> void:
 		if not opened:
 			opened = true
 			var chosen_option = options[randi_range(0, 3)]
-			print(chosen_option)
+			$Sounds/PowerUp.play()
 			if chosen_option == 'heatlh':
 				area.get_parent().increase_health()
 			elif chosen_option == 'bullets':
