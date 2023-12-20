1ca758a18d93692765ad90dca670cf2d49c14f19
JessePires <jessepires2010@gmail.com>
2023-12-20 19:39:47 -0300

    fix: desappear with itemBox when power up is triggered

--- ItemBox/item_box.gd
+++ ItemBox/item_box.gd
@@ -6,8 +6,7 @@ var options = ['heatlh', 'bullets', 'heatlh', 'bullets']
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
-
-
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
@@ -23,3 +22,4 @@ func _on_area_2d_area_entered(area: Area2D) -> void:
 				area.get_parent().increase_health()
 			elif chosen_option == 'bullets':
 				area.get_parent().increase_bullets()
+			queue_free()
